2a6fa2cb9b2804f867a66e997b56059ff7b1cddc
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2019-08-25 12:28:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -105,7 +105,7 @@ async function getBackgroundImg(){
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
