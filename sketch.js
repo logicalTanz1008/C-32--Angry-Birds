eb08e0bb7b716ec5f16ce7a36001d8fc9ff7f060
Tanzeelur Rahman <rahman.tanzeel@gmail.com>
2020-08-26 15:12:44 +0530

    Uploaded Files

--- sketch.js
+++ sketch.js
@@ -99,7 +99,7 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
